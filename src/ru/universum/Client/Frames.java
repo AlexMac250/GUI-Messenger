370fe810b8283b6af9e4f58bd403b85a2b615a94
Зверь <perun1999@mail.ru>
2016-09-26 18:55:13 -0700

    FIX

--- src/ru/universum/Client/Frames.java
+++ src/ru/universum/Client/Frames.java
@@ -218,7 +218,9 @@ class Frames {
 
                 currentFriend = null;
                 frame = new JFrame("NEOnline - Сообщения (v0.1 alpha 1)");
-                if (System.getProperty("os.name").equals("Mac OS X")) frame.setSize(683, 340); else frame.setSize(755, 500);
+               // if (System.getProperty("os.name").equals("Mac OS X"))
+                    frame.setSize(683, 340);
+                //else frame.setSize(755, 500);
                 frame.setResizable(true);                                // FIXME: 20.09.16 resizable
                 contentPain = frame.getContentPane();
                 panFriends = new JPanel();
